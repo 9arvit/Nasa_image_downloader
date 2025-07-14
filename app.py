bd1f89207675cda1127289786306786ceb0b2d0d
Garvit Babbar <100927382+9arvit@users.noreply.github.com>
2025-07-14 21:49:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+inport os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def index():
     return render_template("asteroid.html", results=results, query=query)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=False, host="0.0.0.0", port=port)
